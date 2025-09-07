39f04865d5ca8680afa142573f8264e069e6da3c
okimindan <84608687+okimindan@users.noreply.github.com>
2025-09-07 19:26:53 +0900

    Create footer.php

---
+++ partials/footer.php
@@ -0,0 +1,11 @@
+</main>
+
+<footer class="site-footer">
+  <div class="container">
+    <p>&copy; <?php echo date('Y'); ?> Kaito. All rights reserved.</p>
+  </div>
+</footer>
+
+<script src="assets/script.js"></script>
+</body>
+</html>
